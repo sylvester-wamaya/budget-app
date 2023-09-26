a4d2d194c372c440f95848120f71afe8772042f5
sylvester wamaya <sylvesterwamaya@gmail.com>
2023-09-27 02:34:00 +0300

    Implement payment controller

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,17 +1,16 @@
 class PaymentsController < ApplicationController
   before_action :set_payment, only: %i[show edit update destroy]
-
-  # GET /payments or /payments.json
   def index
     @payments = Payment.all
   end
-
-  # GET /payments/1 or /payments/1.json
-  def show; end
+  def show
+    @payment = Payment.find(params[:id])
+  end
 
   # GET /payments/new
   def new
-    @payment = Payment.new
+    @group = Group.find(params[:group_id])
+    @payment = @group.payments.new
   end
 
   # GET /payments/1/edit
@@ -19,11 +18,13 @@ class PaymentsController < ApplicationController
 
   # POST /payments or /payments.json
   def create
-    @payment = Payment.new(payment_params)
+    @group = Group.find(params[:group_id])
+    @payment = @group.payments.new(payment_params)
+    @payment.user = current_user
 
     respond_to do |format|
       if @payment.save
-        format.html { redirect_to payment_url(@payment), notice: 'Payment was successfully created.' }
+        format.html { redirect_to group_url(@group), notice: 'Payment was successfully created.' }
         format.json { render :show, status: :created, location: @payment }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -47,10 +48,11 @@ class PaymentsController < ApplicationController
 
   # DELETE /payments/1 or /payments/1.json
   def destroy
+   
     @payment.destroy
 
     respond_to do |format|
-      format.html { redirect_to payments_url, notice: 'Payment was successfully destroyed.' }
+      format.html { redirect_to group_payments_url, notice: 'Payment was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
@@ -64,6 +66,6 @@ class PaymentsController < ApplicationController
 
   # Only allow a list of trusted parameters through.
   def payment_params
-    params.require(:payment).permit(:name, :amount, :user_id)
+    params.require(:payment).permit(:name, :amount)
   end
 end
