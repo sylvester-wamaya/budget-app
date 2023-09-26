dde9fb48599d43f0ed59c2864c84faf7b39c2f32
sylvester wamaya <sylvesterwamaya@gmail.com>
2023-09-27 02:35:19 +0300

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :payments
-  resources :groups
+  
+  resources :groups do 
+    resources :payments
+  end
   resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
